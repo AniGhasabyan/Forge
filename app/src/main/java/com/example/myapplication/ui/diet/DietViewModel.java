a7ac4fa219d1648de3e3ab8ca7e062cee919aec8
AniGhasabyan <gasabyanani@gmail.com>
2024-01-19 16:41:54 +0400

    .

---
+++ app/src/main/java/com/example/myapplication/ui/diet/DietViewModel.java
@@ -0,0 +1,20 @@
+package com.example.myapplication.ui.diet;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class DietViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public DietViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is diet fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+
+}
---
+++ app/src/main/java/com/example/myapplication/ui/progress/ProgressViewModel.java
@@ -0,0 +1,19 @@
+package com.example.myapplication.ui.progress;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class ProgressViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public ProgressViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is progression fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
