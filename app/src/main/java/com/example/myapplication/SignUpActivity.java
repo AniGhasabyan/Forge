c210711aea512fdffdb0ed70bee33425ba31dd66
Ani Ghasabyan <gasabyanani@gmail.com>
2024-03-13 15:14:51 +0300

    Commit message

--- app/src/main/java/com/example/myapplication/SignUpActivity.java
+++ app/src/main/java/com/example/myapplication/SignUpActivity.java
@@ -2,25 +2,106 @@ package com.example.myapplication;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseUser;
+
 public class SignUpActivity extends AppCompatActivity {
+
+    private EditText et_username, et_email, et_password1, et_password2;
+    private static final String TAG = "SignUpActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
-        Button signupButton = findViewById(R.id.loginLink);
+        Button loginButton = findViewById(R.id.loginLink);
+        Button signupButton = findViewById(R.id.signUp);
+        et_username = findViewById(R.id.signup_username);
+        et_email = findViewById(R.id.signup_email);
+        et_password1 = findViewById(R.id.signup_password);
+        et_password2 = findViewById(R.id.signup_password_conf);
 
-        signupButton.setOnClickListener(new View.OnClickListener() {
+        loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(SignUpActivity.this, LoginActivity.class);
                 startActivity(intent);
             }
         });
+
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String username = et_username.getText().toString();
+                String email = et_email.getText().toString();
+                String password1 = et_password1.getText().toString();
+                String password2 = et_password2.getText().toString();
+
+                if (TextUtils.isEmpty(username)){
+                    et_username.setError("Username is required");
+                    et_username.requestFocus();
+                } else if (TextUtils.isEmpty(email)){
+                    et_email.setError("Email is required");
+                    et_email.requestFocus();
+                } else  if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                    et_email.setError("Valid email is required");
+                    et_email.requestFocus();
+                } else if (password1.length()<6){
+                    et_password1.setError("Password must be >5 characters");
+                    et_password1.requestFocus();
+                } else if (!password1.equals(password2)) {
+                    et_password2.setError("The Password Doesn't Match");
+                    et_password2.requestFocus();
+                } else {
+                    registerUser(username, email, password1);
+                }
+            }
+        });
+    }
+
+    private void registerUser(String username, String email, String password){
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(SignUpActivity.this,
+                new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            FirebaseUser firebaseUser = auth.getCurrentUser();
+
+                            firebaseUser.sendEmailVerification();
+
+                            Dialog dialog = new Dialog(SignUpActivity.this);
+                            dialog.showDialog("Confirm registration", "");
+                        } else {
+                            try {
+                                throw task.getException();
+                            } catch (FirebaseAuthUserCollisionException e){
+                                et_email.setError("Email address already in use. Please use a different email.");
+                                et_email.requestFocus();
+                            }catch (Exception e){
+                                Log.e(TAG, e.getMessage());
+                                Toast.makeText(SignUpActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    }
+                });
     }
 }
