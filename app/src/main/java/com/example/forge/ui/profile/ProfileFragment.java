320637c7affc504b3ee8ed73fe78321c23625139
Ani Ghasabyan <gasabyanani@gmail.com>
2024-03-26 12:38:16 +0300

    Commit message

--- app/src/main/java/com/example/forge/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/example/forge/ui/profile/ProfileFragment.java
@@ -1,19 +1,26 @@
 package com.example.forge.ui.profile;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.forge.LoginActivity;
+import com.example.forge.MainActivity;
+import com.example.forge.R;
 import com.example.forge.databinding.FragmentProfileBinding;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ProfileFragment extends Fragment {
 
     private FragmentProfileBinding binding;
+    private FirebaseAuth auth;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -22,9 +29,22 @@ public class ProfileFragment extends Fragment {
 
         binding = FragmentProfileBinding.inflate(inflater, container, false);
 
+        auth = FirebaseAuth.getInstance();
+
+        Button logoutButton = binding.getRoot().findViewById(R.id.buttonLogout);
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                auth.signOut();
+                Intent intent = new Intent(requireActivity(), LoginActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+                requireActivity().finish();
+            }
+        });
+
         return binding.getRoot();
     }
-
     @Override
     public void onDestroyView() {
         super.onDestroyView();
