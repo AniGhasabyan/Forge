3faae1b7fed5b40b3dbe588306148edaeb2c0e77
Ani Ghasabyan <gasabyanani@gmail.com>
2024-04-01 21:46:09 +0300

    Commit message

--- app/src/main/java/com/example/forge/ui/navbar/diet/DietViewModel.java
+++ app/src/main/java/com/example/forge/ui/navbar/diet/DietViewModel.java
@@ -5,6 +5,7 @@ import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import com.example.forge.Message;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,6 +13,7 @@ import java.util.List;
 public class DietViewModel extends ViewModel {
     private MutableLiveData<List<Message>> dietNotes;
     private MutableLiveData<String> mText;
+    private FirebaseFirestore db;
 
     public DietViewModel() {
         dietNotes = new MutableLiveData<>();
@@ -19,6 +21,8 @@ public class DietViewModel extends ViewModel {
 
         mText = new MutableLiveData<>();
         mText.setValue("This is diet fragment");
+
+        db = FirebaseFirestore.getInstance();
     }
 
     public LiveData<List<Message>> getDietNotes() {
@@ -30,6 +34,9 @@ public class DietViewModel extends ViewModel {
         if (currentNotes != null) {
             currentNotes.add(0, note);
             dietNotes.setValue(currentNotes);
+
+            db.collection("diets")
+                    .add(note);
         }
     }
 
