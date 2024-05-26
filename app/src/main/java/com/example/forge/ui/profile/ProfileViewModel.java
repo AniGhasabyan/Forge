8b3c521b46333a8de9a8993966b924a15ae4ee09
Ani Ghasabyan <gasabyanani@gmail.com>
2024-05-26 09:48:42 +0300

    Commit message

--- app/src/main/java/com/example/forge/ui/profile/ProfileViewModel.java
+++ app/src/main/java/com/example/forge/ui/profile/ProfileViewModel.java
@@ -1,8 +1,5 @@
 package com.example.forge.ui.profile;
 
-import static androidx.core.content.ContentProviderCompat.requireContext;
-import static androidx.core.content.ContextCompat.startActivity;
-
 import android.app.Activity;
 import android.app.Application;
 import android.content.Intent;
@@ -15,18 +12,25 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 
 import com.example.forge.LoginActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.Tasks;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ProfileViewModel extends AndroidViewModel {
 
-    private FirebaseAuth auth;
-    private FirebaseFirestore db;
-    private StorageReference storageRef;
-
-    private MutableLiveData<String> profileImageUrl = new MutableLiveData<>();
+    private final FirebaseAuth auth;
+    private final FirebaseFirestore db;
+    private final StorageReference storageRef;
+    private final MutableLiveData<String> profileImageUrl = new MutableLiveData<>();
 
     public ProfileViewModel(@NonNull Application application) {
         super(application);
@@ -41,14 +45,11 @@ public class ProfileViewModel extends AndroidViewModel {
 
     public void uploadImageToFirebaseStorage(Uri imageUri) {
         StorageReference imageRef = storageRef.child("profile_images/" + auth.getCurrentUser().getUid());
-
         imageRef.putFile(imageUri)
-                .addOnSuccessListener(taskSnapshot -> {
-                    imageRef.getDownloadUrl().addOnSuccessListener(uri -> {
-                        String imageUrl = uri.toString();
-                        saveImageUrlToFirestore(imageUrl);
-                    });
-                });
+                .addOnSuccessListener(taskSnapshot -> imageRef.getDownloadUrl().addOnSuccessListener(uri -> {
+                    String imageUrl = uri.toString();
+                    saveImageUrlToFirestore(imageUrl);
+                }));
     }
 
     private void saveImageUrlToFirestore(String imageUrl) {
@@ -60,26 +61,59 @@ public class ProfileViewModel extends AndroidViewModel {
 
     public void loadProfilePicture() {
         StorageReference imageRef = storageRef.child("profile_images/" + auth.getCurrentUser().getUid());
-
-        imageRef.getDownloadUrl().addOnSuccessListener(uri -> {
-            String imageUrl = uri.toString();
-            profileImageUrl.setValue(imageUrl);
-        });
+        imageRef.getDownloadUrl().addOnSuccessListener(uri -> profileImageUrl.setValue(uri.toString()));
     }
 
     public void deleteUserAccount(Activity activity) {
-        auth.getCurrentUser().delete()
-                .addOnCompleteListener(task -> {
-                    if (task.isSuccessful()) {
-                        auth.signOut();
-                        Intent intent = new Intent(activity, LoginActivity.class);
-                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                        activity.startActivity(intent);
-                        activity.finish();
-                    } else {
-                        Toast.makeText(activity, "Failed to delete account", Toast.LENGTH_SHORT).show();
-                    }
+        String userId = auth.getCurrentUser().getUid();
+
+        db.collection("users").document(userId).delete()
+                .addOnSuccessListener(aVoid -> {
+                    deleteUserFromCollections(userId, task -> {
+                        if (task.isSuccessful()) {
+                            auth.getCurrentUser().delete()
+                                    .addOnCompleteListener(task1 -> {
+                                        if (task1.isSuccessful()) {
+                                            auth.signOut();
+                                            Intent intent = new Intent(activity, LoginActivity.class);
+                                            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                            activity.startActivity(intent);
+                                            activity.finish();
+                                        } else {
+                                            Toast.makeText(activity, "Failed to delete account", Toast.LENGTH_SHORT).show();
+                                        }
+                                    });
+                        } else {
+                            Toast.makeText(activity, "Failed to delete user data from Firestore", Toast.LENGTH_SHORT).show();
+                        }
+                    });
                 });
+    }
+
+    private void deleteUserFromCollections(String userId, OnCompleteListener<Void> onCompleteListener) {
+        String[] collections = new String[]{
+                "Your Coaches",
+                "Coaches Requested to Train You",
+                "Coaches You're Interested in",
+                "Your Athletes",
+                "Athletes Interested in Your Coaching",
+                "Your Coaching Requests"
+        };
+
+        db.collection("users").get().continueWithTask(task -> {
+            if (!task.isSuccessful()) {
+                return Tasks.forException(task.getException());
+            }
+
+            List<Task<Void>> deleteTasks = new ArrayList<>();
+            for (DocumentSnapshot userDoc : task.getResult()) {
+                for (String collection : collections) {
+                    deleteTasks.add(userDoc.getReference().collection(collection).document(userId).delete());
+                }
+            }
+
+            return Tasks.whenAll(deleteTasks);
+        }).addOnCompleteListener(onCompleteListener);
     }
 
     public void logoutUser(Activity activity) {
@@ -89,6 +123,4 @@ public class ProfileViewModel extends AndroidViewModel {
         activity.startActivity(intent);
         activity.finish();
     }
-
 }
-
